c35e6cf6c8a231014557257ed96cfbf365bb134a
Md_Ajharul_Islam <167879829+MdAjharulIslam@users.noreply.github.com>
2024-05-10 19:40:34 +0600

    Add files via upload

--- string.c
+++ string.c
@@ -1,13 +1,23 @@
 #include<stdio.h>
 int main(){
 
-char a[]="md ajharul islam";
+char a[100];
+gets(a);
+char ch;
+int len=0;
+for(int i=0;a[i]!='0';i++){
+    len++;
+}
+for(int i=0;i<=len;i++){
 
-char b[]="this is my string";
+    if (a[i]==ch){
+        continue;
+    }
 
-puts(a);
-puts(b);
-
+    printf("%c",a[i]);
+    ch=a[i];
+}
+printf("\n");
 
 
 
